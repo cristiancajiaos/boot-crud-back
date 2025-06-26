739f47d283b3cc5703ba6b7717d06fd2c57dc450
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-26 10:17:39 -0400

    Primer commit

---
+++ src/main/java/com/example/bootcrudback/BootCrudBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootcrudback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCrudBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootCrudBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/bootcrudback/BootCrudBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootcrudback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCrudBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
