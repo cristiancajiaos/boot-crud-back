00e9cab3b4a7e0c65442897f6067c26bdaeef378
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-27 11:35:10 -0400

    Habilitadas solicitudes CORS para la app de Front

--- src/main/java/com/example/bootcrudback/BootCrudBackApplication.java
+++ src/main/java/com/example/bootcrudback/BootCrudBackApplication.java
@@ -2,12 +2,27 @@ package com.example.bootcrudback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BootCrudBackApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BootCrudBackApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/employees").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/employee").allowedOrigins("http://localhost:4200");
+      }
+    };
+
   }
 
 }
